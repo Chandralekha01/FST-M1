9bea7a95e9dfe0f9da19a2975902b565178fbfc1
Chandralekha01 <91046033+Chandralekha01@users.noreply.github.com>
2022-04-08 17:23:00 +0530

    Add files via upload

---
+++ SQL/Activities/SQL1.sql
@@ -0,0 +1,10 @@
+Create database FSTactivites;
+CREATE TABLE salesman  
+( 
+SALESMAN_ID int NOT NULL, 
+SALESMAN_NAME varchar(32), 
+SALESMAN_CITY varchar(32), 
+COMMISSION int  
+);
+
+describe salesman
---
+++ SQL/Activities/SQL4.sql
@@ -0,0 +1,19 @@
+elect salesman_id , commission  from salesman 
+where salesman_name = 'Paul Adam';
+
+alter table salesman 
+add grade int ;
+
+alter table salesman 
+add grade int ;
+
+alter table salesman 
+add grade int ;
+
+update salesman  
+set grade = 100 ;
+
+select * from salesman;
+
+
+
